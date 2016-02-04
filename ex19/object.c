b21f7579f044652cd1aa944421e54584f36ea239
Mark Snidal <mark.snidal@gmail.com>
2016-02-03 22:06:13 -0500

    Added ex19

---
+++ ex19/object.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "object.h"
+#include <assert.h>
+
+void Object_destroy(void *self)
+{
+    Object *obj = self;
+
+    if (obj) {
+        if (obj->description) free (obj->description);
+        free(obj);
+    }
+}
+
+void Object_describe(void *self)
+{
+    Object *obj = self;
+    printf("%s.\n", obj->description);
+}
+
+int Object_init(void *self)
+{
+    // Do nothing for now (js style object system)
+    return 1;
+}
+
+void *Object_move(void *self, Direction direction)
+{
+    printf("You can't go that direction.\n");
+    return NULL;
+}
+
+int Object_attack(void *self, int damage)
+{
+    printf("You can't attack that.\n");
+    return 0;
+}
+
+void *Object_new(size_t size, Object proto, char *description)
+{
+    // Set up the default functions if they aren't set
+    if (!proto.init) proto.init = Object_init;
+    if (!proto.describe) proto.describe = Object_describe;
+    if (!proto.destroy) proto.destroy = Object_destroy;
+    if (!proto.attack) proto.attack = Object_attack;
+    if (!proto.move) proto.move = Object_move;
+
+    // Struct of one size, point to a different pointer at it to "cast" it
+    Object *el = calloc(1, size);
+    *el = proto;
+
+    // Copy the description over
+    el->description = strdup(description);
+
+    // Initialize it with whatever init we were given
+    if (!el->init(el)) {
+        el->destroy(el);
+        return NULL;
+    } else {
+        return el;
+    }
+}
+
